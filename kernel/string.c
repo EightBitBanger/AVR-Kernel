819a49d7d1bdfd9fdfb6af91ad9031db34568ce0
EightBitBanger <66331916+EightBitBanger@users.noreply.github.com>
2026-05-17 18:03:18 -0600

    Added x86 platform target

--- kernel/string.c
+++ kernel/string.c
@@ -1,5 +1,82 @@
 #include <kernel/string.h>
-#include <stdint.h>
+
+static inline int is_delimiter(char c, const char *delim) {
+    while (*delim) {
+        if (c == *delim) {
+            return 1;
+        }
+        delim++;
+    }
+    return 0;
+}
+
+size_t strlen(const char* str) {
+    const char *s = str;
+    while (*s != '\0') {
+        s++;
+    }
+    return s - str;
+}
+
+char* strcpy(char *dest, const char* src) {
+    char *ptr = dest;
+    while ((*dest++ = *src++) != '\0');
+    return ptr;
+}
+
+int strcmp(const char* str1, const char* str2) {
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    return *(const unsigned char*)str1 - *(const unsigned char*)str2;
+}
+
+char* strtok(char* str, const char* delim) {
+    static char *last_token = NULL;
+    if (str != NULL) 
+        last_token = str;
+    
+    if (last_token == NULL || *last_token == '\0') 
+        return NULL;
+    
+    while (*last_token && is_delimiter(*last_token, delim)) 
+        last_token++;
+    
+    if (*last_token == '\0') {
+        last_token = NULL;
+        return NULL;
+    }
+    
+    char* token_start = last_token;
+    while (*last_token && !is_delimiter(*last_token, delim)) {
+        last_token++;
+    }
+    
+    if (*last_token != '\0') {
+        *last_token = '\0'; // Overwrite delimiter with null terminator
+        last_token++;       // Move past the null terminator for the next call
+    } else {
+        last_token = NULL;  // End of string reached
+    }
+    
+    return token_start;
+}
+
+void* memset(void* s, int c, size_t n) {
+    unsigned char* p = s;
+    while (n--) 
+        *p++ = (unsigned char)c;
+    return s;
+}
+
+void* memcpy(void* dest, const void* src, size_t n) {
+    unsigned char* d = dest;
+    const unsigned char* s = src;
+    while (n--) 
+        *d++ = *s++;
+    return dest;
+}
 
 void utos(uint32_t value, char* dest) {
     char buffer[12];
--- kernel/string.h
+++ kernel/string.h
@@ -1,4 +1,13 @@
 #include <stdint.h>
+#include <stddef.h>
+
+size_t strlen(const char* str);
+char* strcpy(char* dest, const char* src);
+int strcmp(const char* str1, const char* str2);
+char* strtok(char* str, const char* delim);
+
+void* memset(void* s, int c, size_t n);
+void* memcpy(void* dest, const void* src, size_t n);
 
 void itos(int32_t value, char* dest);
 void utos(uint32_t value, char* dest);
